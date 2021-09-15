9087286e4ea528031dfc4824df7655bd2c2b3abc
jbeecy <john.e.beecy@gmail.com>
2021-09-15 10:39:39 -0400

    add inquiry model
    This commit adds the inquiry model for use in routing.

---
+++ app/models/inquiry.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const inquirySchema = new mongoose.Schema({
+  subject: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Inquiry', inquirySchema)
